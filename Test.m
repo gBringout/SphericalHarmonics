a6d71014f340fcbd8f7214b400a53f55ed125be3
Gael Bringout <gael.bringout@gmail.com>
2015-03-22 11:40:45 +0100

    Updated for IWMPI demo

--- Test.m
+++ Test.m
@@ -1,5 +1,4 @@
-%% This files aimed at provinding an example for the use of the spherical
-% harmonics for the fields
+%% This files provides an example for the use of spherical harmonics
 
 clear all
 close all
@@ -19,20 +18,20 @@ rk = createTargetPointGaussLegendreAndRectangle7(rhoReference,degreeMax,orderMax
 
 
 %% the bc and bs coefficietn are inizialized for each field direction
-bc(1).coefficient = zeros(degreeMax,orderMax);
-bc(2).coefficient = zeros(degreeMax,orderMax);
-bc(3).coefficient = zeros(degreeMax,orderMax);
-bs(1).coefficient = zeros(degreeMax,orderMax);
-bs(2).coefficient = zeros(degreeMax,orderMax);
-bs(3).coefficient = zeros(degreeMax,orderMax);
+bc(1).coefficient = zeros(degreeMax+1,orderMax+1);
+bc(2).coefficient = zeros(degreeMax+1,orderMax+1);
+bc(3).coefficient = zeros(degreeMax+1,orderMax+1);
+bs(1).coefficient = zeros(degreeMax+1,orderMax+1);
+bs(2).coefficient = zeros(degreeMax+1,orderMax+1);
+bs(3).coefficient = zeros(degreeMax+1,orderMax+1);
 
 % we decide to try with a field topology as:
 % this is arbitrary
 bc(1).coefficient(1,1) = 0.78;
-bc(1).coefficient(2,2) = 1;
+bs(1).coefficient(2,2) = 1;
 bc(1).coefficient(3,1) = 2;
-bc(1).coefficient(5,1) = 3;
-bc(1).coefficient(7,1) = 4;
+bc(2).coefficient(5,1) = pi;
+bc(3).coefficient(7,1) = 4;
 
 %% we calculate the value of the modeled field on the point of a sphere
 B  = RebuildField7bis(bc,bs,rhoReference,rk,'sch');
@@ -43,19 +42,22 @@ displayFieldOnSphere(B,rk,'Field calculated from our topology')
 % we should get exactly the same number, as we are making back an expansion
 % with the correctorder
 [bc2,bs2] = getSphericalHarmonicsCoefficientMeasure7(B(1,:,:),B(2,:,:),B(3,:,:),degreeMax,orderMax,rk,'sch');
-disp(sprintf('%g\n',bc2(1).coefficient(1,1)))
-disp(sprintf('%g\n',bc2(1).coefficient(2,2)))
-disp(sprintf('%g\n',bc2(1).coefficient(3,1)))
-disp(sprintf('%g\n',bc2(1).coefficient(5,1)))
-disp(sprintf('%g\n Done\n',bc2(1).coefficient(7,1)))
+disp(sprintf('%g',bc2(1).coefficient(1,1)))
+disp(sprintf('%g',bs2(1).coefficient(2,2)))
+disp(sprintf('%g',bc2(1).coefficient(3,1)))
+disp(sprintf('%g',bc2(2).coefficient(5,1)))
+disp(sprintf('%g \n Done\n',bc2(3).coefficient(7,1)))
 
 % we can then display them
 displaySHC(bc2,bs2,2,1)
 
-% and reconstruct the field in a 2D plan
+% and reconstruct the Bx field ampltiude in a 2D plan
 x = -rhoReference:rhoReference/50:rhoReference;
 y = -rhoReference:rhoReference/50:rhoReference;
 z= 0;
 B  = RebuildField7(bc,bs,rhoReference,x,y,z,'sch');
 Bx = squeeze(B(1,:,:));
-figure;imagesc(x,y,Bx);
+figure
+imagesc(x,y,Bx);
+axis image
+title('x component of the magentic field')
